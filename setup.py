cf30d0cd24dd70f894933ccc06275d695a01edae
Alexander J Ritter <48734959+ajritter8@users.noreply.github.com>
2024-04-27 08:50:26 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='junctionCounts',
-    version='0.1.0',
+    version='1.0.0',
     packages=['junctionCounts',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('pyREADME.txt').read(),
